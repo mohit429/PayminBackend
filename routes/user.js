44938269e7007558bb7897f1b6215e7850efea72
mohit429 <128832823+mohit429@users.noreply.github.com>
2024-06-29 18:49:26 +0530

    updated

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const zod = require("zod");
 const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
-const { authMiddleware } = require("../middleware");
+const { authMiddleware } = require("../BACKEND/middleware");
 
 const signupBody = zod.object({
     username: zod.string().email(),
