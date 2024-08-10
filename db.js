347f0e6d8dc7787dc876e6c6d0d5a0211a31d8e4
mohit429 <128832823+mohit429@users.noreply.github.com>
2024-08-10 10:02:02 +0530

    db.js & acoount.js Updated

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://mohitgurbani429:hRePFvaSsAMruGFQ@cluster0.z3jwdyk.mongodb.net/paytm");
 
-// Create a Schema for Users
+// User Schema
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -33,6 +33,7 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Account Schema
 const accountSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId, // Reference to User model
@@ -45,10 +46,35 @@ const accountSchema = new mongoose.Schema({
     }
 });
 
-const Account = mongoose.model('Account', accountSchema);
+// Transaction Schema
+const transactionSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
 const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+const Transaction = mongoose.model('Transaction', transactionSchema);
 
 module.exports = {
-	User,
-    Account
+    User,
+    Account,
+    Transaction
 };
--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const { authMiddleware } = require('../middleware');
-const { Account } = require('../db');
+const { Account,Transaction} = require('../db');
 const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
@@ -24,13 +24,11 @@ router.get("/balance", authMiddleware, async (req, res) => {
 
 router.post("/transfer", authMiddleware, async (req, res) => {
     const session = await mongoose.startSession();
-
     session.startTransaction();
+
     const { amount, to } = req.body;
 
-    // Fetch the accounts within the transaction
     const account = await Account.findOne({ userId: req.userId }).session(session);
-
     if (!account || account.balance < amount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -39,7 +37,6 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     }
 
     const toAccount = await Account.findOne({ userId: to }).session(session);
-
     if (!toAccount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -47,15 +44,36 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         });
     }
 
-    // Perform the transfer
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
     await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
 
-    // Commit the transaction
+    // Log the transaction
+    const transaction = new Transaction({
+        from: req.userId,
+        to,
+        amount
+    });
+    await transaction.save({ session });
+
     await session.commitTransaction();
     res.status(200).json({
         message: "Transfer successful"
     });
+});
+
+router.get("/history", authMiddleware, async (req, res) => {
+    try {
+        const transactions = await Transaction.find({
+            $or: [
+                { from: req.userId },
+                { to: req.userId }
+            ]
+        }).populate('from to', 'username').sort({ date: -1 });
+
+        res.status(200).json(transactions);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching transaction history" });
+    }
 });
 
 module.exports = router;
